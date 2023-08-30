ef45a91f3a9db3ee79cfc2b04a2b944e23ccee76
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2023-08-31 01:36:38 +0200

    Do basic HTML, CSS and password check

---
+++ script.js
@@ -0,0 +1,23 @@
+const password = document.querySelector("#user_password");
+const confirmPassword = document.querySelector("#confirm_password");
+const passwordAlert = document.querySelector("#password-confirmation");
+const submitButton = document.querySelector("#submit-button");
+
+function checkPassword() {
+    if (password.value !== confirmPassword.value) {
+        passwordAlert.textContent = "*Passwords don't match";
+        passwordAlert.style.color = "red";
+        submitButton.disabled = true;
+    } else {
+        passwordAlert.textContent = "";
+        submitButton.disabled = false;
+    }
+}
+
+password.addEventListener("input", () => {
+    checkPassword();
+})
+
+confirmPassword.addEventListener("input", () => {
+    checkPassword();
+})
